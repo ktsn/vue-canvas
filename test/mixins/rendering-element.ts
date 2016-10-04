b8ad376a3a8f9c8d8b802ab14485da68a0afc513
ktsn <ktsn55@gmail.com>
2016-10-05 02:10:39 +0900

    Add rendering element mixin test

---
+++ test/mixins/rendering-element.ts
@@ -0,0 +1,64 @@
+import * as assert from 'power-assert'
+import * as sinon from 'sinon'
+import * as Vue from 'vue'
+import renderingElementMixin from '../../src/mixins/rendering-element'
+
+describe('Rendering Element Mixin', () => {
+  it('must dispatch update event to context', done => {
+    const Target = {
+      mixins: [renderingElementMixin],
+      props: {
+        test: Number
+      }
+    }
+
+    const vm: any = new Vue({
+      data: {
+        test: 1
+      },
+      render (this: any, h) {
+        return h(Target, { props: { test: this.test }})
+      }
+    }).$mount()
+
+    // dependent event bus
+    vm.eventBus = new Vue()
+    const spy = sinon.spy()
+    vm.eventBus.$on('update', spy)
+
+    vm.test = 2
+    Vue.nextTick(() => {
+      assert(spy.called)
+      done()
+    })
+  })
+
+  it('does not notify update if there are no props update', done => {
+    const Target = {
+      mixins: [renderingElementMixin],
+      props: {
+        test: Number
+      }
+    }
+
+    const vm: any = new Vue({
+      data: {
+        test: 1
+      },
+      render (this: any, h) {
+        return h(Target, { props: { test: this.test }})
+      }
+    }).$mount()
+
+    // dependent event bus
+    vm.eventBus = new Vue()
+    const spy = sinon.spy()
+    vm.eventBus.$on('update', spy)
+
+    vm.test = 1
+    Vue.nextTick(() => {
+      assert(!spy.called)
+      done()
+    })
+  })
+})
