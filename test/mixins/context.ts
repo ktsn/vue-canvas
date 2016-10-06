8706c7736f8e1b112601983327311c9293abff2b
ktsn <ktsn55@gmail.com>
2016-10-07 01:15:45 +0900

    Add test to check mounting canvas element

--- test/mixins/context.ts
+++ test/mixins/context.ts
@@ -10,13 +10,30 @@ describe('Context Mixin', () => {
     clearRect () {}
   }
 
+  it('mount canvas element with specifying width and height', () => {
+    const vm = new Vue({
+      mixins: [contextMixin],
+      propsData: {
+        width: 500,
+        height: 400
+      },
+      canvas: {
+        getContext: () => mockCtx
+      }
+    }).$mount()
+
+    const el: any = vm.$el
+    assert(el instanceof HTMLCanvasElement)
+    assert(el.width === 500)
+    assert(el.height === 400)
+  })
+
   it('observes all updates and renders only once', done => {
     const vm: any = new Vue({
       mixins: [contextMixin],
       canvas: {
         getContext: () => mockCtx
-      },
-      render
+      }
     })
 
     const spy = sinon.spy(vm, 'render')
