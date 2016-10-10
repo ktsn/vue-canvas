1fb11cf731a513cc3156925cba063f7f1826c82b
ktsn <ktsn55@gmail.com>
2016-10-10 14:32:46 +0900

    Update basic example

--- examples/basic/App.vue
+++ examples/basic/App.vue
@@ -1,34 +1,56 @@
 <template lang="html">
   <div>
     <context-2d :width="300" :height="300">
-      <rectangle :fill="true" :x="x" :y="y" :width="width" :height="height"></rectangle>
+      <rectangle fill :fill-style="a.color" :x="a.x" :y="a.y" :width="a.width" :height="a.height"></rectangle>
+      <property :stroke-style="b.color">
+        <rectangle stroke :x="b.x" :y="b.y" :width="b.width" :height="b.height"></rectangle>
+      </property>
     </context-2d>
     <div class="">
       <div>
-        x <input type="number" v-model="x">
-        y <input type="number" v-model="y">
-        width <input type="number" v-model="width">
-        height <input type="number" v-model="height">
+        x <input type="number" v-model="a.x">
+        y <input type="number" v-model="a.y">
+        width <input type="number" v-model="a.width">
+        height <input type="number" v-model="a.height">
+        color <input type="color" v-model="a.color">
+      </div>
+      <div>
+        x <input type="number" v-model="b.x">
+        y <input type="number" v-model="b.y">
+        width <input type="number" v-model="b.width">
+        height <input type="number" v-model="b.height">
+        color <input type="color" v-model="b.color">
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Context2d, Rectangle } from '../../src'
+import { Context2d, Property, Rectangle } from '../../src'
 
 export default {
   data () {
     return {
-      x: 20,
-      y: 10,
-      width: 150,
-      height: 120
+      a: {
+        x: 20,
+        y: 10,
+        width: 150,
+        height: 120,
+        color: '#00ffff'
+      },
+      b: {
+        x: 50,
+        y: 50,
+        width: 50,
+        height: 50,
+        color: '#0000ff'
+      }
     }
   },
 
   components: {
     Context2d,
+    Property,
     Rectangle
   }
 }
